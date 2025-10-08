f00f1562039ed078e1aa8dfe996c41d5814e434b
Severe77 <maityanubhab2007@gmail.com>
2025-10-08 13:44:28 +0530

    fix(EbookReader): internal hyperlinks work in Firefox local mode (#1429)

--- activities/EbookReader.activity/js/activity.js
+++ activities/EbookReader.activity/js/activity.js
@@ -36,43 +36,35 @@ var app = new Vue({
   },
 
   mounted: function () {
-    // Load last library from Journal
     var vm = this;
     requirejs(
       ["sugar-web/activity/activity", "sugar-web/env"],
       function (activity, env) {
         env.getEnvironment(function (err, environment) {
           // Use buddy color for background
-          env.getEnvironment(function (err, environment) {
-            document.getElementById("canvas").style.backgroundColor =
-              environment.user.colorvalue.fill;
-          });
+          document.getElementById("canvas").style.backgroundColor =
+            environment.user.colorvalue.fill;
 
           // Load context
           if (environment.objectId) {
-            activity
-              .getDatastoreObject()
-              .loadAsText(function (error, metadata, data) {
-                if (error == null && data != null) {
-                  var parsed = JSON.parse(data);
-                  vm.currentLibrary = parsed.library;
-                  if (parsed.current !== undefined) {
-                    vm.currentBook = vm.currentLibrary.database[parsed.current];
-                    vm.currentEpub = ePub(
-                      vm.currentLibrary.information.fileprefix +
-                        vm.currentBook.file
-                    );
-                    vm.currentView = EbookReader;
-                  } else if (vm.currentLibrary.database.length == 0) {
-                    vm.loadLibrary(defaultUrlLibrary);
-                  }
-                  document.getElementById("spinner").style.visibility =
-                    "hidden";
+            activity.getDatastoreObject().loadAsText(function (error, metadata, data) {
+              if (!error && data) {
+                var parsed = JSON.parse(data);
+                vm.currentLibrary = parsed.library;
+                if (parsed.current !== undefined) {
+                  vm.currentBook = vm.currentLibrary.database[parsed.current];
+                  vm.currentEpub = ePub(
+                    vm.currentLibrary.information.fileprefix + vm.currentBook.file
+                  );
+                  vm.currentView = EbookReader;
+                } else if (vm.currentLibrary.database.length === 0) {
+                  vm.loadLibrary(defaultUrlLibrary);
                 }
-              });
+                document.getElementById("spinner").style.visibility = "hidden";
+              }
+            });
           } else {
             vm.loadLibrary(defaultUrlLibrary);
-            // Fix in case of accidental exit
             vm.saveContextToJournal();
           }
         });
@@ -84,12 +76,10 @@ var app = new Vue({
       vm.onResize();
     });
 
-    // Handle unfull screen buttons (b)
-    document
-      .getElementById("unfullscreen-button")
-      .addEventListener("click", function () {
-        vm.unfullscreen();
-      });
+    // Handle unfullscreen button
+    document.getElementById("unfullscreen-button").addEventListener("click", function () {
+      vm.unfullscreen();
+    });
   },
 
   updated: function () {
@@ -116,7 +106,7 @@ var app = new Vue({
           document.getElementById("spinner").style.visibility = "hidden";
           document.getElementById("cloudwarning").style.visibility = "hidden";
         })
-        .catch(function (error) {
+        .catch(function () {
           document.getElementById("spinner").style.visibility = "hidden";
           document.getElementById("cloudwarning").style.visibility = "visible";
         });
@@ -136,10 +126,8 @@ var app = new Vue({
       this.saveContext();
       if (this.currentView === EbookReader) {
         this.currentView = LibraryViewer;
-      } else {
-        if (this.currentBook) {
-          this.currentView = EbookReader;
-        }
+      } else if (this.currentBook) {
+        this.currentView = EbookReader;
       }
     },
 
@@ -147,81 +135,62 @@ var app = new Vue({
       this.$refs.view.goToPage(this.currentEpub.contents);
     },
 
-    // Handle fullscreen mode
     fullscreen: function () {
       document.getElementById("main-toolbar").style.opacity = 0;
       document.getElementById("canvas").style.top = "0px";
-      document.getElementById("unfullscreen-button").style.visibility =
-        "visible";
-      if (this.currentView === EbookReader) {
-        var reader = this.$refs.view;
-        reader.render(this.currentEpub, reader.getLocation());
-      }
-    },
-    unfullscreen: function () {
-      document.getElementById("main-toolbar").style.opacity = 1;
-      document.getElementById("canvas").style.top = "55px";
-      document.getElementById("unfullscreen-button").style.visibility =
-        "hidden";
+      document.getElementById("unfullscreen-button").style.visibility = "visible";
       if (this.currentView === EbookReader) {
         var reader = this.$refs.view;
         reader.render(this.currentEpub, reader.getLocation());
       }
     },
 
-    // Handling popup settings
+    unfullscreen: function () {
+      document.getElementById("main-toolbar").style.opacity = 1;
+      document.getElementById("canvas").style.top = "55px";
+      document.getElementById("unfullscreen-button").style.visibility = "hidden";
+      if (this.currentView === EbookReader) {
+        var reader = this.$refs.view;
+        reader.render(this.currentEpub, reader.getLocation());
+      }
+    },
+
     setLibraryUrl: function () {
       var titleOk = this.$refs.localization.get("Ok"),
         titleCancel = this.$refs.localization.get("Cancel"),
         titleSettings = this.$refs.localization.get("Settings"),
         titleUrl = this.$refs.localization.get("Url");
+
       this.$refs.settings.show({
-        content:
-          `
-					<div id='popup-toolbar' class='toolbar' style='padding: 0'>
-						<button class='toolbutton pull-right' id='popup-ok-button' title='` +
-          titleOk +
-          `' style='outline:none;background-image: url(lib/sugar-web/graphics/icons/actions/dialog-ok.svg)'></button>
-						<button class='toolbutton pull-right' id='popup-cancel-button' title='` +
-          titleCancel +
-          `' style='outline:none;background-image: url(lib/sugar-web/graphics/icons/actions/dialog-cancel.svg)'></button>
-						<div style='position: absolute; top: 20px; left: 60px;'>` +
-          titleSettings +
-          `</div>
-					</div>
-					<div id='popup-container' style='width: 100%; overflow:auto'>
-						<div class='popup-label'>` +
-          titleUrl +
-          `</div>
-						<input id='input' class='popup-input'/>
-					</div>`,
+        content: `
+          <div id='popup-toolbar' class='toolbar' style='padding: 0'>
+            <button class='toolbutton pull-right' id='popup-ok-button' title='${titleOk}' style='outline:none;background-image: url(lib/sugar-web/graphics/icons/actions/dialog-ok.svg)'></button>
+            <button class='toolbutton pull-right' id='popup-cancel-button' title='${titleCancel}' style='outline:none;background-image: url(lib/sugar-web/graphics/icons/actions/dialog-cancel.svg)'></button>
+            <div style='position: absolute; top: 20px; left: 60px;'>${titleSettings}</div>
+          </div>
+          <div id='popup-container' style='width: 100%; overflow:auto'>
+            <div class='popup-label'>${titleUrl}</div>
+            <input id='input' class='popup-input'/>
+          </div>
+        `,
         closeButton: false,
-        modalStyles: {
-          backgroundColor: "white",
-          height: "160px",
-          width: "600px",
-          maxWidth: "90%",
-        },
+        modalStyles: { backgroundColor: "white", height: "160px", width: "600px", maxWidth: "90%" },
       });
     },
+
     settingsShown: function () {
       var vm = this;
       document.getElementById("popup-container").style.height =
-        document.getElementById("popup-toolbar").parentNode.offsetHeight -
-        55 * 2 +
-        "px";
-      document
-        .getElementById("popup-ok-button")
-        .addEventListener("click", function () {
-          vm.$refs.settings.close(true);
-        });
-      document
-        .getElementById("popup-cancel-button")
-        .addEventListener("click", function () {
-          vm.$refs.settings.close();
-        });
+        document.getElementById("popup-toolbar").parentNode.offsetHeight - 110 + "px";
+      document.getElementById("popup-ok-button").addEventListener("click", function () {
+        vm.$refs.settings.close(true);
+      });
+      document.getElementById("popup-cancel-button").addEventListener("click", function () {
+        vm.$refs.settings.close();
+      });
       document.getElementById("input").value = defaultUrlLibrary;
     },
+
     settingsClosed: function (result) {
       if (result) {
         this.loadLibrary(document.getElementById("input").value);
@@ -229,10 +198,8 @@ var app = new Vue({
       this.$refs.settings.destroy();
     },
 
-    // Handle events
     onBookSelected: function (book) {
       if (this.currentView === LibraryViewer) {
-        // Load book
         var vm = this;
         vm.currentBook = book;
         Vue.set(book, "spinner", true);
@@ -249,36 +216,31 @@ var app = new Vue({
             function () {
               vm.currentEpub.loaded.navigation.then(function (sections) {
                 sections.forEach(function (section) {
-                  if (section.label.toUpperCase().trim() == "CONTENTS") {
+                  if (section.label.toUpperCase().trim() === "CONTENTS") {
                     vm.currentEpub.contents = section.href;
                   }
                 });
               });
               vm.currentView = EbookReader;
               book.spinner = false;
+
+              // Firefox internal hyperlink fix
               const readerContainer = document.getElementById("reader");
               if (readerContainer) {
                 readerContainer.addEventListener("click", function (e) {
                   const target = e.target;
-                  if (
-                    target.tagName === "A" &&
-                    target.getAttribute("href").startsWith("#")
-                  ) {
+                  if (target.tagName === "A" && target.getAttribute("href").startsWith("#")) {
                     e.preventDefault();
                     const targetId = target.getAttribute("href").substring(1);
                     const targetEl = document.getElementById(targetId);
-                    if (targetEl) {
-                      targetEl.scrollIntoView({ behavior: "smooth" });
-                    }
+                    if (targetEl) targetEl.scrollIntoView({ behavior: "smooth" });
                   }
                 });
               }
             },
             function () {
               console.log(
-                "Error loading " +
-                  vm.currentLibrary.information.fileprefix +
-                  vm.currentBook.file
+                "Error loading " + vm.currentLibrary.information.fileprefix + vm.currentBook.file
               );
               book.spinner = false;
             }
@@ -292,9 +254,7 @@ var app = new Vue({
       var vm = this;
       if (vm.currentView === EbookReader) {
         var reader = vm.$refs.view;
-        if (this.timer) {
-          window.clearTimeout(this.timer);
-        }
+        if (this.timer) window.clearTimeout(this.timer);
         this.timer = window.setTimeout(function () {
           vm.currentBook.location = reader.getLocation();
           reader.render(vm.currentEpub, vm.currentBook.location);
@@ -306,17 +266,12 @@ var app = new Vue({
     onHelp: function () {
       var options = {};
       options.switchbutton = this.$refs.toolbar.$refs.switchbutton.$el;
-      options.contentsbutton = null;
-      if (this.$refs.toolbar.$refs.contentsbutton) {
-        options.contentsbutton = this.$refs.toolbar.$refs.contentsbutton.$el;
-      }
+      options.contentsbutton = this.$refs.toolbar.$refs.contentsbutton
+        ? this.$refs.toolbar.$refs.contentsbutton.$el
+        : null;
       options.settingsbutton = this.$refs.toolbar.$refs.settings.$el;
       options.fullscreenbutton = this.$refs.toolbar.$refs.fullscreen.$el;
-      if (
-        this.currentView === LibraryViewer &&
-        this.$refs.view.$refs.item0 &&
-        this.$refs.view.$refs.item0[0]
-      ) {
+      if (this.currentView === LibraryViewer && this.$refs.view.$refs.item0 && this.$refs.view.$refs.item0[0]) {
         options.book = this.$refs.view.$refs.item0[0].$el;
       } else if (this.currentView === EbookReader) {
         options.prevbutton = document.getElementById("left");
@@ -326,28 +281,19 @@ var app = new Vue({
     },
 
     saveContextToJournal: function () {
-      // Save current library in Journal on Stop
       var vm = this;
       vm.saveContext();
       requirejs(["sugar-web/activity/activity"], function (activity) {
         console.log("writing...");
         var context = {
-          library: {
-            information: vm.currentLibrary.information,
-            database: vm.currentLibrary.database,
-          },
+          library: { information: vm.currentLibrary.information, database: vm.currentLibrary.database },
         };
-        if (vm.currentView === EbookReader) {
-          context.current = vm.currentLibrary.database.indexOf(vm.currentBook);
-        }
+        if (vm.currentView === EbookReader) context.current = vm.currentLibrary.database.indexOf(vm.currentBook);
         var jsonData = JSON.stringify(context);
         activity.getDatastoreObject().setDataAsText(jsonData);
         activity.getDatastoreObject().save(function (error) {
-          if (error === null) {
-            console.log("write done.");
-          } else {
-            console.log("write failed.");
-          }
+          if (!error) console.log("write done.");
+          else console.log("write failed.");
         });
       });
     },
